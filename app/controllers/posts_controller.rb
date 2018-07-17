3900286823aee3c822ec9ddc08c7322e67e63a78
Neil Seejoor <nvseejoor@gmail.com>
2018-07-17 20:13:59 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,5 +27,10 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 
-	
+	private
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
+	end
+
 end
